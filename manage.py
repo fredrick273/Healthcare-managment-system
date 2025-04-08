6e37d39c1a4d22d5685ddf03f5a0b0491318c1cc
Sam Fredrick <samfredrick273@gmail.com>
2025-04-09 02:58:10 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 # test 123
 # final clone test
 # offffff
-# HOPE IS THERE AGAIN THIS TIME
+# HOPE IS THERE AGAIN THIS TIME FINALLY
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
